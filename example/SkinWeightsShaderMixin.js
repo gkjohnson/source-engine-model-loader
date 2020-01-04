1afc0a6f16724538e0cbef21ce0536dad03c569d
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-01-04 00:10:14 -0800

    add skin weight shader

---
+++ example/SkinWeightsShaderMixin.js
@@ -0,0 +1,62 @@
+import { Color, UniformsUtils } from 'three';
+
+function cloneShader(shader, uniforms, defines) {
+
+	const newShader = Object.assign({}, shader);
+	newShader.uniforms = UniformsUtils.merge([
+		newShader.uniforms,
+		uniforms
+	]);
+	newShader.defines = Object.assign({}, defines);
+
+	return newShader;
+
+}
+
+export function SkinWeightMixin(shader) {
+	const defineKeyword = 'ENABLE_SKIN_WEIGHTS';
+	const newShader = cloneShader(
+		shader,
+		{
+			skinWeightColor: { value: new Color() },
+			skinWeightOpacity: { value: 1.0 },
+			skinWeightIndex: { value: 6 }
+		},
+		{
+			[defineKeyword]: 1,
+		},
+	);
+
+	newShader.vertexShader = `
+		uniform float skinWeightIndex;
+		varying float skinWeightColorRatio;
+		${newShader.vertexShader}
+	`.replace(
+		/#include <skinning_vertex>/,
+		v => `${v}
+		{
+			#ifdef ENABLE_SKIN_WEIGHTS
+				skinWeightColorRatio += skinWeight.x * float(skinIndex.x == skinWeightIndex);
+				skinWeightColorRatio += skinWeight.y * float(skinIndex.y == skinWeightIndex);
+				skinWeightColorRatio += skinWeight.z * float(skinIndex.z == skinWeightIndex);
+				skinWeightColorRatio += skinWeight.w * float(skinIndex.w == skinWeightIndex);
+			#endif
+		}
+		`
+	)
+
+	newShader.fragmentShader = `
+			uniform vec3 skinWeightColor;
+			varying float skinWeightColorRatio;
+			${newShader.fragmentShader}
+		`.replace(
+			/vec4 diffuseColor = vec4\( diffuse, opacity \);/,
+			v => `${v}
+			#ifdef ENABLE_SKIN_WEIGHTS
+				diffuseColor = vec4(skinWeightColor, skinWeightColorRatio * opacity);
+			#endif
+			`,
+		);
+
+	return newShader;
+}
